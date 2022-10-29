91de687d52b9bf60b8e8df84fa5b8e59aa1677b3
Mohit <workwithmokrus@gmail.com>
2022-10-29 22:31:32 +0530

    new changes

--- sanity.ts
+++ sanity.ts
@@ -5,8 +5,7 @@ export const config = {
    dataset : process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
    projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
    apiVersion : "2021-03-25",
-   useCdn : false,
-   ignoreBrowserTokenWarning: true,
+   useCdn : process.env.NODE_ENV === "production",
 }
 
 export const sanityClient = createClient(config);
